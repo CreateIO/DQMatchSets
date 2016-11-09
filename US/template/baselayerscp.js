104445ddb8bc01148101a46e3d2a7174b0217dbf
Dustin <dustin@creat-re.com>
2016-11-09 11:43:19 -0800

    a script version

---
+++ US/template/baselayerscp.js
@@ -0,0 +1,109 @@
+var BASELAYERJSON = {
+    "baselayer": {
+        "Emerald": {
+            "inUse": true,
+            "type": "osm",
+            "name": "emerald",
+            "url": "//a.tiles.mapbox.com/v3/zooombear.nc05cb2j",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://www.openstreetmap.org/copyright",
+                "text": "© Mapbox, Data ODbL © OpenStreetMap contributors"
+            }
+        },
+        "Satellite": {
+            "inUse": true,
+            "type": "osm",
+            "name": "satellite",
+            "url": "//a.tiles.mapbox.com/v3/zooombear.libl3g28",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://www.openstreetmap.org/copyright",
+                "text": "© Mapbox, Data ODbL © OpenStreetMap contributors"
+            }
+        },
+        "Streets": {
+            "inUse": true,
+            "type": "osm",
+            "name": "streets",
+            "url": "//a.tiles.mapbox.com/v3/zooombear.libjp8ng",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://www.openstreetmap.org/copyright",
+                "text": "© Mapbox, Data ODbL © OpenStreetMap contributors"
+            }
+        },
+        "Contour": {
+            "inUse": true,
+            "type": "osm",
+            "name": "contour",
+            "url": "//a.tiles.mapbox.com/v3/zooombear.ld5cm6bl",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "",
+                "text": ""
+            }
+        },
+        "Positron": {
+            "inUse": false,
+            "type": "url",
+            "name": "Positron",
+            "url": "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://cartodb.com/attributions",
+                "text": "Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+            }
+        },
+        "Toner": {
+            "inUse": true,
+            "type": "osm",
+            "name": "toner",
+            "url": "//stamen-tiles.a.ssl.fastly.net/toner-lite/",
+            "fileExtension": "png",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://stamen.com/",
+                "text": "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA."
+            }
+        },
+        "Watercolor": {
+            "inUse": true,
+            "type": "url",
+            "name": "watercolor",
+            "url": "//stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
+            "fileExtension": "jpg",
+            "maximumLevel": 18,
+            "credit": {
+                "imageUrl": "",
+                "link": "http://stamen.com/",
+                "text": "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA."
+            }
+        },
+        "Historical": {
+            "inUse": true,
+            "type": "osm",
+            "name": "historical",
+            "url": "//a.tiles.mapbox.com/v3/zooombear.k0k5jei3",
+            "fileExtension": "jpg",
+            "maximumLevel": 17,
+            "credit": {
+                "imageUrl": "",
+                "link": "",
+                "text": ""
+            }
+        }
+    }
+};
+
